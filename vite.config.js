baae2d2686577e76ecc09870529ace5ea8edcc1f
Sania Irshad <saniairshad110@gmail.com>
2025-05-27 06:19:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/My-Portfolio/',  // 👈 EXACTLY this, matching your repo name
+  base: '/My-Portfolio/',
   plugins: [react()],
 });
